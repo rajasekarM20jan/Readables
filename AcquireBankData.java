4a65c15fde11ed2df304bdb65ec498eabdb45bff
rajasekarM20jan <114177445+rajasekarM20jan@users.noreply.github.com>
2022-09-22 22:56:01 +0530

    Add files via upload

--- AcquireBankData.java
+++ AcquireBankData.java
@@ -1,25 +1,34 @@
-import java.io.FileReader;
-import java.util.ArrayList;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+//--------------Business Logic------------
+//import statements for
+import java.io.FileReader; //1. Reading any file (here : JSON)
+import java.util.ArrayList; //2. Creating Arraylist for POJO class constructor.
+import org.json.simple.JSONArray; //3. Creating Json list/array
+import org.json.simple.JSONObject; //4. Creating object for json which formats for json type
+import org.json.simple.parser.JSONParser; //5. parsing json file into the class
 
 public class AcquireBankData{
 
-
+    //creating constructor for class AcquireBankData
     AcquireBankData() throws Exception{
+        // parsing json data into regular class object
         Object oj = new JSONParser().parse(new FileReader("C:\\Users\\raj\\IdeaProjects\\rajasekar\\src\\AccountDetails.json"));
+        //converting object into array of json format
         JSONArray j= (JSONArray) oj;
+        //converting each json array index objects into respective jsonobjects
         JSONObject first= (JSONObject) j.get(0);
         JSONObject second= (JSONObject) j.get(1);
         JSONObject third= (JSONObject) j.get(2);
+        //creating array named details for storing json objects
         JSONObject[] details = {first,second,third};
+        //creating new arraylist for pojo Bank
         ArrayList<Bank> arr= new ArrayList<>();
+        //using for loop for pushing data to Bank
         for(int i=0;i<details.length;i++) {
             arr.add(new Bank((String)details[i].get("name"),(String)details[i].get("pin"),(String)details[i].get("typeOfAccount"),(String)details[i].get("dateOfBirth"),(String)details[i].get("balance")));
         }
+        //using for loop for pulling data from Bank
         for(int k=0;k<details.length;k++){
+            //using getter methods from Bank for pulling data
             String nameFetcher= arr.get(k).getName();
             int pinFetcher = Integer.parseInt(arr.get(k).getPin());
             String accountTypeFetcher = arr.get(k).getTypeOfAccount();
@@ -31,8 +40,9 @@ public class AcquireBankData{
             System.out.println("Balance : " +balanceFetcher);
         }
     }
-
+    //------------Execution Logic-------------
     public static void main(String[] args) throws Exception {
+        //calling constructor AcquireBankData for operation executions.
         new AcquireBankData();
     }
 }
--- Bank.java
+++ Bank.java
@@ -1,17 +1,22 @@
+//--------------Business Logic------------
+//Creation of POJO class
 public class Bank {
+    //Initializing the global variables for POJO class named Bank
     String name;
     String pin;
     String typeOfAccount;
     String dateOfBirth;
     String balance;
-
+    //creating a parameterized constructor for getting the data from JSON Object from child class AcquireBankData
     public Bank(String name,String pin,String typeOfAccount,String dateOfBirth,String balance) {
+        //Assigning whatever the data got into class variables using "this" keyword
         this.name=name;
         this.pin=pin;
         this.typeOfAccount=typeOfAccount;
         this.dateOfBirth=dateOfBirth;
         this.balance=balance;
     }
+    //creating get method for class variables with return type.
     public String getName() {
         return name;
     }
